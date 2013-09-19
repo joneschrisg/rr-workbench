d7a13815a025fa5d3223e79a33a21e2a696b4f86
Chris Jones <cjones.bugs@gmail.com>
2013-09-19 13:50:17 -0700

    Testing positive invalid syscalls

--- bad_syscall.c
+++ bad_syscall.c
@@ -4,8 +4,11 @@
 int
 main(int argc, char** argv)
 {
-    puts("entering syscall(-42) ...");
-    syscall(-42, 1, 2, 3, 4, 5, 6);
+    const int bad_syscall = 500;
+
+    puts("entering bad syscall ...");
+    syscall(bad_syscall, 1, 2, 3, 4, 5, 6);
     puts("done");
+
     return 0;
 }
