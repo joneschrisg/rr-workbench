b89b5998a98038a87aca8b0379c986250e3d5b6e
Chris Jones <cjones.bugs@gmail.com>
2014-02-18 18:25:29 -0800

    Make monkeypatch code closer to production quality.

--- vdso_monkeypatch.c
+++ vdso_monkeypatch.c
@@ -1,5 +1,7 @@
+#include <assert.h>
 #include <errno.h>
 #include <linux/limits.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -36,65 +38,167 @@ static const byte vsyscall_impl[] = {
     0xc3,                       /* ret */
 };
 
+/* NBB: the placeholder bytes in |struct insns_template| below must be
+ * kept in sync with this. */
 static const byte vsyscall_monkeypatch[] = {
     0x50,                         /* push %eax */
-    0xb8, 0xc0, 0x86, 0x04, 0x08, /* mov $vsyscall_hook_trampoline, %eax */
+    0xb8, 0x00, 0x00, 0x00, 0x00, /* mov $_vsyscall_hook_trampoline, %eax */
+    /* The immediate param of the |mov| is filled in dynamically by
+     * the template mechanism below.  The NULL here is a
+     * placeholder. */
     0xff, 0xe0,                   /* jmp *%eax */
 };
 
-__asm__(".text\n\t"
-        "vsyscall_hook_trampoline:\n\t"
-        "pop %eax\n\t"
-        "push %ebp\n\t"
-        "push %edi\n\t"
-        "push %esi\n\t"
-        "push %edx\n\t"
-        "push %ecx\n\t"
-        "push %ebx\n\t"
-        "push %eax\n\t"
-        "mov $vsyscall_hook, %eax\n\t"
-        "call *%eax\n\t"
-        "addl $4, %esp\n\t"
-        "pop %ebx\n\t"
-        "pop %ecx\n\t"
-        "pop %edx\n\t"
-        "pop %esi\n\t"
-        "pop %edi\n\t"
-        "pop %ebp\n\t"
-        "ret\n\t"
-    );
+struct insns_template {
+    /* NBB: |vsyscall_monkeypatch| must be kept in sync with these
+     * placeholder bytes. */
+    byte push_eax_insn;
+    byte mov_vsyscall_hook_trampoline_eax_insn;
+    void* vsyscall_hook_trampoline_addr;
+} __attribute__((packed));
 
-static int holy_crap_it_worked;
-static long last_syscallno;
-static long last_a0;
-static long last_ret;
+typedef union {
+    byte bytes[sizeof(vsyscall_monkeypatch)];
+    struct insns_template insns;
+} __attribute__((packed)) monkeypatch_template;
+
+/**
+ * Save the syscall params to this struct so that we can pass around a
+ * pointer to it and avoid pushing/popping all this data for calls to
+ * subsequent handlers.
+ */
+struct syscall_info {
+    long no;
+    long args[6];
+};
+
+__asm__(".text\n\t"
+        ".globl _vsyscall_hook_trampoline\n\t"
+        ".type _vsyscall_hook_trampoline, @function\n\t"
+        "_vsyscall_hook_trampoline:\n\t"
+	".cfi_startproc\n\t"
+
+        /* The monkeypatch pushed $eax on the stack, but there's no
+         * CFI info for it.  Fix up the CFA offset here to account for
+         * the monkeypatch code. */
+        ".cfi_adjust_cfa_offset 4\n\t"
+        ".cfi_rel_offset %eax, 0\n\t"
+
+        /* Pull $eax back off the stack.  Now our syscall-arg
+         * registers are restored to their state on entry to
+         * __kernel_vsyscall(). */
+        "popl %eax\n\t"
+	".cfi_adjust_cfa_offset -4\n\t"
+	".cfi_restore %eax\n\t"
+
+        /* Build a |struct syscall_info| by pushing all the syscall
+         * args and the number onto the stack. */
+                                /* struct syscall_info info; */
+        "pushl %ebp\n\t"        /* info.args[5] = $ebp; */
+	".cfi_adjust_cfa_offset 4\n\t"
+	".cfi_rel_offset %ebp, 0\n\t"
+        "pushl %edi\n\t"        /* info.args[4] = $edi; */
+	".cfi_adjust_cfa_offset 4\n\t"
+	".cfi_rel_offset %edi, 0\n\t"
+        "pushl %esi\n\t"        /* info.args[3] = $esi; */
+	".cfi_adjust_cfa_offset 4\n\t"
+	".cfi_rel_offset %esi, 0\n\t"
+        "pushl %edx\n\t"        /* info.args[2] = $edx; */
+	".cfi_adjust_cfa_offset 4\n\t"
+	".cfi_rel_offset %edx, 0\n\t"
+        "pushl %ecx\n\t"        /* info.args[1] = $ecx; */
+	".cfi_adjust_cfa_offset 4\n\t"
+	".cfi_rel_offset %ecx, 0\n\t"
+        "pushl %ebx\n\t"        /* info.args[0] = $ebx; */
+	".cfi_adjust_cfa_offset 4\n\t"
+	".cfi_rel_offset %ebx, 0\n\t"
+        "pushl %eax\n\t"        /* info.no = $eax; */
+	".cfi_adjust_cfa_offset 4\n\t"
+
+        /* $esp points at &info.  Push that pointer on the stack as
+         * our arg for syscall_hook(). */
+        "movl %esp, %ecx\n\t"
+        "pushl %ecx\n\t"
+	".cfi_adjust_cfa_offset 4\n\t"
+
+        "movl $vsyscall_hook, %eax\n\t"
+        "call *%eax\n\t"        /* $eax = vsyscall_hook(&info); */
+
+        /* $eax is now the syscall return value.  Erase the |&info|
+         * arg and |info.no| from the stack so that we can restore the
+         * other registers we saved. */
+        "addl $8, %esp\n\t"
+	".cfi_adjust_cfa_offset -8\n\t"
+
+        /* Contract of __kernel_vsyscall() is that even callee-save
+         * registers aren't touched, so we restore everything here. */
+        "popl %ebx\n\t"
+	".cfi_adjust_cfa_offset -4\n\t"
+	".cfi_restore %ebx\n\t"
+        "popl %ecx\n\t"
+	".cfi_adjust_cfa_offset -4\n\t"
+	".cfi_restore %ecx\n\t"
+        "popl %edx\n\t"
+	".cfi_adjust_cfa_offset -4\n\t"
+	".cfi_restore %edx\n\t"
+        "popl %esi\n\t"
+	".cfi_adjust_cfa_offset -4\n\t"
+	".cfi_restore %esi\n\t"
+        "popl %edi\n\t"
+	".cfi_adjust_cfa_offset -4\n\t"
+	".cfi_restore %edi\n\t"
+        "popl %ebp\n\t"
+	".cfi_adjust_cfa_offset -4\n\t"
+	".cfi_restore %ebp\n\t"
+
+        /* Return to the caller of *|__kernel_vsyscall()|*, because
+         * the monkeypatch jumped to us. */
+        "ret\n\t"
+    	".cfi_endproc\n\t"
+        ".size _vsyscall_hook_trampoline, .-_vsyscall_hook_trampoline\n\t");
+
+static void* get_vsyscall_hook_trampoline(void)
+{
+    void *ret;
+    __asm__ __volatile__(
+	    "call .L_get_vsyscall_hook_trampoline__pic_helper\n\t"
+	    ".L_get_vsyscall_hook_trampoline__pic_helper: pop %0\n\t"
+	    "addl $(_vsyscall_hook_trampoline - .L_get_vsyscall_hook_trampoline__pic_helper),%0"
+	    : "=a"(ret));
+    return ret;
+}
 
 static long
-vsyscall_hook(long syscallno,
-              long a0, long a1, long a2, long a3, long a4, long a5)
+vsyscall_hook(const struct syscall_info* call)
 {
     long ret;
     __asm__ __volatile__("int $0x80"
                          : "=a"(ret)
-                         : "0"(syscallno), "b"(a0), "c"(a1), "d"(a2),
-                           "S"(a3), "D"(a4));
-
-    holy_crap_it_worked = 1;
-    last_syscallno = syscallno;
-    last_a0 = a0;
-    last_ret = ret;
-
+                         : "0"(call->no), "b"(call->args[0]),
+                           "c"(call->args[1]), "d"(call->args[2]),
+                           "S"(call->args[3]), "D"(call->args[4]));
+    /* *(volatile int*)0 = 42; */
     return ret;
 }
 
 static void
 monkey_patch()
 {
+    monkeypatch_template t;
     byte* orig = (byte*)vsyscall;
     int i;
-    for (i = 0; i < sizeof(vsyscall_monkeypatch); ++i) {
-        orig[i] = vsyscall_monkeypatch[i];
+
+    memcpy(t.bytes, vsyscall_monkeypatch, sizeof(t.bytes));
+    /* Try to catch out-of-sync |vsyscall_monkeypatch| and |struct
+     * insns_template|. */
+    assert(NULL == t.insns.vsyscall_hook_trampoline_addr);
+    t.insns.vsyscall_hook_trampoline_addr = get_vsyscall_hook_trampoline();
+
+    for (i = 0; i < sizeof(t.bytes); ++i) {
+        orig[i] = t.bytes[i];
     }
+    /* If the monkeypatch works, this write() call will be routed
+     * through our vdso vsyscall hook. */
     printf("Successfully monkey-patched vdso to call %p!\n", vsyscall_hook);
 }
 
@@ -137,12 +241,5 @@ main(int argc, char** argv)
 
     mprotect(vdso_start, vdso_num_bytes, PROT_READ | PROT_EXEC);
 
-    if (holy_crap_it_worked) {
-        printf("HOLY CRAP IT WORKED: %ld(0x%lx) -> %ld\n",
-               last_syscallno, last_a0, last_ret);
-    }
-
     return 0;
 }
-
-//0x80486d4 - 0x80486c0
